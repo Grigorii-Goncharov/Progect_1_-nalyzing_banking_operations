cd44816221208cb94b5d24c08608a92a60dd29dc
grigorii goncharov <grigoriy85@gmail.com>
2025-04-27 14:02:17 +0300

    test_get_path_to_file_and_period

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,8 @@
 import pytest
 from unittest.mock import patch
-from src.utils import time_greeting
+from src.utils import time_greeting, get_data_period
 
-# тесты time_greeting
+# Тесты time_greeting
 @pytest.mark.parametrize("hour, expected", [
     (5, "Доброе утро"),
     (11, "Доброе утро"),
@@ -18,3 +18,29 @@ def test_time_greeting(hour, expected):
         mock_datetime.now.return_value.hour = hour
         assert time_greeting() == expected
 
+# Функции тестирования получения периода даты
+
+@pytest.mark.parametrize("input_date, expected_output", [
+    # Проверка обычного случая (май 2020)
+    (
+        "2020-05-20 15:30:00",
+        ["01.05.2020 15:30:00", "20.05.2020 15:30:00"]
+    ),
+    (
+        "2023-01-01 00:00:00",
+        ["01.01.2023 00:00:00", "01.01.2023 00:00:00"]
+    ),
+    (
+        "2025-12-31 23:59:59",
+        ["01.12.2025 23:59:59", "31.12.2025 23:59:59"]
+    ),
+])
+def test_get_data_period(input_date, expected_output):
+    result = get_data_period(input_date)
+    assert result == expected_output
+
+
+def test_get_data_period_invalid_format():
+    # Проверка на некорректный формат даты
+    with pytest.raises(ValueError):
+        get_data_period("20.05.2020")  # Неправильный формат (должен быть "%Y-%m-%d %H:%M:%S")
